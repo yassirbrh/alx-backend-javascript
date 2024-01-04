0e9f392b0bde13b9afa9a5e6ba42d69b41e48089
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-01-04 01:37:53 +0000

    function handleProfileSignup()

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,8 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((resolve) => { console.log(`${resolve[0].body} ${resolve[1].firstName} ${resolve[1].lastName}`); })
+    .catch(() => console.log('Signup system offline'));
+}

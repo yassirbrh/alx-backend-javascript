c2f623fbbecc3e74d1a6f53e165fd83c689ab429
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-01-04 16:34:41 +0000

    Function handleProfileSignup.

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -6,6 +6,6 @@ export default async function handleProfileSignup(firstName, lastName, fileName)
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((results) => (results.map((result) => ({
       status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
+      value: result.status === 'fulfilled' ? result.value : String(result.reason),
     }))));
 }

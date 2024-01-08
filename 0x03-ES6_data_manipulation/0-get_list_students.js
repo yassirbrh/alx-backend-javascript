581d2121307e9e834cd44dcbf5354e86043481ab
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-01-08 09:24:28 +0000

    Function getListStudents that returns an array of objects.

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function getListStudents() {
+	const arr = []
+	arr.push({ id: 1, firstName: 'Guillaume', location: 'San Francisco' });
+	arr.push({ id: 2, firstName: 'James', location: 'Columbia' });
+	arr.push({ id: 5, firstName: 'Serena', location: 'San Francisco' });
+	return arr;
+}

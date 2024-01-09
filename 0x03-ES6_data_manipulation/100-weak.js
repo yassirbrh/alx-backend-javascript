f00b9c012819f578cf2c104a06b8e6e07bb58b3d
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-01-09 19:45:38 +0000

    Exporting the WeakMap object and the function queryAPI.

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,12 +3,12 @@ const weakMap = new WeakMap();
 function queryAPI(endpoint) {
   if (weakMap.get(endpoint) === undefined) {
     weakMap.set(endpoint, 0);
-  } else if (weakMap.get(endpoint) >= 5) {
+  }
+  if (weakMap.get(endpoint) >= 5) {
     throw new Error('Endpoint load is high');
   }
   const value = weakMap.get(endpoint);
   weakMap.set(endpoint, value + 1);
-  return weakMap;
 }
 
 export { weakMap, queryAPI };

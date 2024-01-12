9f986ecc7f0c73b6c51369fe9dc5ddb552b71aae
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-01-12 19:26:26 +0000

    Task 8.

---
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -0,0 +1,13 @@
+/// <reference path="./crud.d.ts" />
+import {RowID, RowElement} from './interface';
+import * as CRUD from './crud';
+
+const row: RowElement = {
+    firstName: "Guillaume",
+    lastName: "Salva",
+};
+const newRowID : RowID = CRUD.insertRow(row);
+const updateRow: RowElement = {...row, age: 23};
+
+CRUD.updateRow(newRowID, updateRow);
+CRUD.deleteRow(newRowID);

ebf332b1d10b9a6f2a8b72ce1049d8ebe08509bf
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-01-13 00:18:01 +0000

    Task 11.

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -2,9 +2,11 @@ export interface MajorCredits {
   credits: number & { __brand: 'MajorCredits.credits' };
 }
 
+
 export interface MinorCredits {
   credits: number & { __brand: 'MinorCredits.credits' };
 }
+
 
 export function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
   return { credits: subject1.credits + subject2.credits } as MajorCredits;

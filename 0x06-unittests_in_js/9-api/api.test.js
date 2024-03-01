09e31664c745b41d842832b06f792c6e0eeaf42e
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-03-01 23:16:03 +0000

    Creating an integration test for the express instance 'app' for the route /cart.

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,7 +1,7 @@
 const request = require('request');
 const { expect } = require('chai');
 
-describe('API test', () => {
+describe('aPI test', () => {
   it('test for GET /', () => new Promise((done) => {
     request.get('http://localhost:7865/', (_err, res, body) => {
       expect(res.statusCode).to.be.equal(200);
@@ -9,22 +9,23 @@ describe('API test', () => {
       done();
     });
   }));
-  it('test for GET /cart/:id for a valid number', (done) => {
+  it('test for GET /cart/:id for a valid number', () => new Promise((done) => {
     request.get('http://localhost:7865/cart/47', (_err, res, body) => {
       expect(res.statusCode).to.be.equal(200);
       expect(body).to.be.equal('Payment methods for cart 47');
       done();
     });
-  });
-  it('test for GET /cart/:id for a negative number', (done) => {
+  }));
+  it('test for GET /cart/:id for a negative number', () => new Promise((done) => {
     request.get('http://localhost:7865/cart/-47', (_err, res, _body) => {
       expect(res.statusCode).to.be.equal(404);
       done();
     });
-  });
-  it('test for GET /cart/:id for a non-numeric value', (done) => {
+  }));
+  it('test for GET /cart/:id for a non-numeric value', () => new Promise((done) => {
     request.get('http://localhost:7865/cart/d200-44a5-9de6', (_err, res, _body) => {
       expect(res.statusCode).to.be.equal(404);
       done();
-    });
+   	});
+  }));
 });

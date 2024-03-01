2c80e5fd59ce4b6fb2bece6969b784c19f50f795
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-03-01 23:08:22 +0000

    Creating an integration test for the express instance 'app' for the route /cart.

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,30 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('API test', () => {
+  it('test for GET /', () => new Promise((done) => {
+    request.get('http://localhost:7865/', (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
+      done();
+    });
+  }));
+  it('test for GET /cart/:id for a valid number', (done) => {
+    request.get('http://localhost:7865/cart/47', (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Payment methods for cart 47');
+      done();
+    });
+  });
+  it('test for GET /cart/:id for a negative number', (done) => {
+    request.get('http://localhost:7865/cart/-47', (_err, res, _body) => {
+      expect(res.statusCode).to.be.equal(404);
+      done();
+    });
+  });
+  it('test for GET /cart/:id for a non-numeric value', (done) => {
+    request.get('http://localhost:7865/cart/d200-44a5-9de6', (_err, res, _body) => {
+      expect(res.statusCode).to.be.equal(404);
+      done();
+    });
+});

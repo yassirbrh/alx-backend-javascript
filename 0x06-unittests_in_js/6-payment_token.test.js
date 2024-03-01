754142bf364fceb642c55a9b758802f4c8f1a98f
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-03-01 12:10:29 +0000

    Creating a test for the function getPaymentTokenFromAPI to test if the promise is returned with an object.

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const { expect } = require('chai');
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', () => {
+  it('getPaymentTokenFromAPI should return Promise if success is true', () => new Promise((done) => {
+    getPaymentTokenFromAPI(true).then((res) => {
+      expect(res).to.deep.equal({ data: 'Successful response from the API' });
+      done();
+    });
+  }));
+});

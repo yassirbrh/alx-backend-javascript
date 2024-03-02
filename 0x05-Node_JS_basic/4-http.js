4f61e18112674cc227a9f9c2ce2bee96da566042
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-03-02 19:12:02 +0000

    Creating a small HTTP server using the http module.

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const http = require('http');
+
+const host = 'localhost';
+const port = 1245;
+
+const app = http.createServer((_, resp) => {
+  const text = 'Hello Holberton School!';
+
+  resp.setHeader('Content-Type', 'text/plain');
+  resp.setHeader('Content-Length', text.length);
+  resp.writeHead(200);
+  resp.write(Buffer.from(text));
+});
+
+app.listen(port, host, () => {
+  process.stdout.write(`Server listening at -> http://${host}:${port}\n`);
+});
